58b2a18f3191eccb409ab9d5c0c25ec05f7bf597
Oleg <evolrn89@gmail.com>
2025-01-21 00:02:40 +0600

    [API] - init app - installed dependencies - configured Db connection

---
+++ forum_team_3/forumApi/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import mongoose from 'mongoose';
+import mongoDb from "./mongoDb";
+import path from "path";
+import config from "./config";
+
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+const run = async () => {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(config.mongoDbPath);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+    process.on('exit', () => {
+        mongoDb.disconnect();
+    });
+};
+
+run().catch(err => console.log(err));
+
+
