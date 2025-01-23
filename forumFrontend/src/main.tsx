20906a7e15786618ddaf408fbb8925d5d9e3b824
Oleg <evolrn89@gmail.com>
2025-01-23 17:50:54 +0600

    KAN-8 [Frontend] Настроено хранилище Добавлен userSlice для хранения данных пользователя Добавлены userThunks для регистрации пользователя, входа и выхода из системы

--- forumFrontend/src/main.tsx
+++ forumFrontend/src/main.tsx
@@ -2,5 +2,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "@fontsource/roboto/cyrillic.css";
+import { persistor, store } from './app/store.ts';
+import { Provider } from 'react-redux';
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+);
