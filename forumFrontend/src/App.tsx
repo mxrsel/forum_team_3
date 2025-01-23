7a70513f3c0874e7053bd8800a3aabfd90f19c49
Oleg <evolrn89@gmail.com>
2025-01-23 18:53:05 +0600

    KAN-9 [Frontend] Создана форма для регистрации пользователя

--- forumFrontend/src/App.tsx
+++ forumFrontend/src/App.tsx
@@ -1,8 +1,7 @@
-
 const App = () => {
     return (
         <>
-            App
+
         </>
     );
 };
--- forumFrontend/src/main.tsx
+++ forumFrontend/src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import "@fontsource/roboto/cyrillic.css";
 import { persistor, store } from './app/store.ts';
