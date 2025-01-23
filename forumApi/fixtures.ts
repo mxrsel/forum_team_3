36bf578c1eb8d07e77450ffc792f4cf1942c9fa6
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2025-01-23 23:14:39 +0600

    KAN-15 фикс фикстур

--- forumApi/fixtures.ts
+++ forumApi/fixtures.ts
@@ -5,7 +5,7 @@ import User from './models/User';
 import { randomUUID } from 'node:crypto';
 import Comment from './models/Comment';
 
-const run = async() => {
+const run = async () => {
   await mongoose.connect(config.mongoDbPath);
   const db = mongoose.connection;
 
@@ -13,32 +13,31 @@ const run = async() => {
     await db.dropCollection('users');
     await db.dropCollection('posts');
     await db.dropCollection('comments');
-  } catch(e) {
+  } catch (e) {
     console.log('Collections were not presents, skipping drop');
   }
 
-   const [John, Maria, Anna, Jake, ] = await User.create(
+  const [John, Maria, Anna, Jake] = await User.create(
     {
       username: 'John',
       password: '123456',
       token: randomUUID(),
     },
-     {
-       username: 'Maria',
-       password: '77777',
-       token: randomUUID(),
-     },
-     {
-       username: 'Anna',
-       password: '8888',
-       token: randomUUID(),
-     },
-     {
-       username: 'Jake',
-       password: '2222',
-       token: randomUUID(),
-     },
-
+    {
+      username: 'Maria',
+      password: '77777',
+      token: randomUUID(),
+    },
+    {
+      username: 'Anna',
+      password: '8888',
+      token: randomUUID(),
+    },
+    {
+      username: 'Jake',
+      password: '2222',
+      token: randomUUID(),
+    }
   );
 
   const [SquidGame, Receipt] = await Post.create(
@@ -47,14 +46,14 @@ const run = async() => {
       postTitle: 'What do you think about new season of Squid Game?',
       postContent: 'In my opinion this season is so interesting',
       postImage: 'fixtures/John.png',
-      datetime: new Date().toDateString()
+      datetime: new Date().toDateString(),
     },
     {
       user: Maria._id,
       postTitle: 'Can you give me some ideas for present for my friend ',
       postContent: 'Want to surprise her',
       postImage: 'fixtures/Maria.png',
-      datetime: new Date().toDateString()
+      datetime: new Date().toDateString(),
     }
   );
 
@@ -62,7 +61,8 @@ const run = async() => {
     {
       user: John._id,
       post: Receipt._id,
-      commentText: 'If you want to surprise your friend listen call of your soul and choose',
+      commentText:
+        'If you want to surprise your friend listen call of your soul and choose',
     },
     {
       user: Anna._id,
@@ -78,7 +78,8 @@ const run = async() => {
       user: Jake._id,
       post: SquidGame._id,
       commentText: 'For me first season is better',
-    },
-  )
-}
+    }
+  );
+};
 
+run().catch(console.error);
